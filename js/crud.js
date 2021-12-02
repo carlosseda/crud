98d8d839dbdcd83f4e2b939248a62b50345e4452
carlosseda <carlossedagambin@gmail.com>
2021-12-02 21:33:18 +0100

    change axios

--- js/crud.js
+++ js/crud.js
@@ -46,9 +46,34 @@ export let renderForm = () => {
                     //     }
                     // });
 
-                    let response = await axios.post(url, data).then(response => {
+
+                    let response = await axios.post(url, data, {
+                        headers: {
+                            'Authorization': 'Basic tony_admin@laravel.com:$2y$10$D3vG45dtYiR3O/d46x4pveWLqBZoxQWuq7..fbvOxNvZnW7eMWoOm',
+                            'Content-Type': 'application/json',
+                            "Access-Control-Allow-Origin": "*"
+                        }
+                    }).then(response => {
                         
                         console.log(response);
+
+                    }).catch(error => {
+                        
+                        if(error.response.status == '422'){
+
+                            let errors = error.response.data.errors;      
+                            let errorMessage = '';
+
+                            Object.keys(errors).forEach(function(key) {
+                                errorMessage += '<li>' + errors[key] + '</li>';
+                            })
+
+                            console.log(errorMessage);
+                        }
+
+                        if(error.response.status == '500'){
+                            console.log(error);
+                        }
                     });
                 };
         
