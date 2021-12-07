d2298b2e62568155dd4520e98ab7eb95e1f59ef3
carlosseda <carlossedagambin@gmail.com>
2021-12-08 00:28:48 +0100

    fetch

--- js/form.js
+++ js/form.js
@@ -12,77 +12,56 @@ export let renderForm = () => {
             event.preventDefault();
     
             forms.forEach(form => { 
-                
-                var myHeaders = new Headers();
 
-                var raw = JSON.stringify({
-                    "name": "carlos",
-                    "email": "carlossedagambin@gmail.com",
-                    "password": "temporal",
-                    "c_password": "temporal"
+                let data = new FormData(form);
+                data.append("name", "carlos");
+                data.append("email", "cedagambin@gmail.com");
+                data.append("password", "temporal");
+                data.append("c_password", "temporal");
+                data.append("fingerprint", getFingerprint());
+
+                let object = {};
+
+                data.forEach(function(value, key){
+                    object[key] = value;
                 });
 
-                var requestOptions = {
-                    method: 'POST',
-                    headers: myHeaders,
-                    body: raw,
-                    redirect: 'follow'
-                };
+                let json = JSON.stringify(object);
 
-                fetch("http://141.94.27.118:8080/api/register", requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error => console.log('error', error));
+                console.log(json);
 
-                // let data = new FormData(form);
-                // data.append("name", "carlos");
-                // data.append("email", "cedagambin@gmail.com");
-                // data.append("password", "temporal");
-                // data.append("c_password", "temporal");
-                // data.append("fingerprint", getFingerprint());
-
-                // let object = {};
-
-                // data.forEach(function(value, key){
-                //     object[key] = value;
-                // });
-
-                // let json = JSON.stringify(object);
-
-                // console.log(json);
-
-                // let url = form.action;
+                let url = form.action;
     
-                // let sendPostRequest = async () => {
+                let sendPostRequest = async () => {
             
-                //     // let response = await fetch(url, {
-                //     //     method: 'POST', 
-                //     //     mode: 'cors', 
-                //     //     headers: {
-                //     //         'Accept': '*/*',
-                //     //         'Content-Type': 'application/json',
-                //     //     },
-                //     //     body: json
-                //     // }).then(function(response){
-                //     //     console.log(response);
-                //     // }).catch(function(error) {
+                    let response = await fetch(url, {
+                        method: 'POST', 
+                        mode: 'no-cors', 
+                        headers: {
+                            'Accept': '*/*',
+                            'Content-Type': 'application/json',
+                        },
+                        body: json
+                    }).then(function(response){
+                        console.log(response);
+                    }).catch(function(error) {
 
-                //     //     if(error.response.status == '400'){
+                        if(error.response.status == '400'){
     
-                //     //         let errors = error.response.data.errors;      
-                //     //         let errorMessage = '';
+                            let errors = error.response.data.errors;      
+                            let errorMessage = '';
         
-                //     //         Object.keys(errors).forEach(function(key) {
-                //     //             errorMessage += '<li>' + errors[key] + '</li>';
-                //     //         })
+                            Object.keys(errors).forEach(function(key) {
+                                errorMessage += '<li>' + errors[key] + '</li>';
+                            })
             
-                //     //         console.log(errorMessage);
-                //     //     }
+                            console.log(errorMessage);
+                        }
     
-                //     //     if(error.response.status == '500'){
-                //     //         console.log(error);
-                //     //     }
-                //     // });
+                        if(error.response.status == '500'){
+                            console.log(error);
+                        }
+                    });
 
 
                 //     let response = await axios.post(url, json).then(response => {
@@ -107,9 +86,9 @@ export let renderForm = () => {
                 //             console.log(error);
                 //         }
                 //     });
-                // };
+                };
         
-                // sendPostRequest();
+                sendPostRequest();
             });
         });
     }
