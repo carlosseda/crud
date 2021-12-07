4d87b6e5132bd93debb1ac5992a55d3e6629e09e
carlosseda <carlossedagambin@gmail.com>
2021-12-07 21:42:05 +0100

    axios

--- js/form.js
+++ js/form.js
@@ -20,65 +20,59 @@ export let renderForm = () => {
     
                 let sendPostRequest = async () => {
             
-                    let response = await fetch(url, {
-                        method: 'POST', 
-                        mode: 'no-cors', 
-                        headers: {
-                            'Content-Type': 'multipart/form-data',
-                            "Access-Control-Allow-Origin": "*",
-                            "X-Requested-With": "XMLHttpRequest"
-                        },
-                        body: JSON.stringify(data)
-                    }).then(function(response){
-                        console.log(response);
-                    }).catch(function(error) {
+                //     let response = await fetch(url, {
+                //         method: 'POST', 
+                //         mode: 'no-cors', 
+                //         headers: {
+                //             'Content-Type': 'multipart/form-data',
+                //             "Access-Control-Allow-Origin": "*",
+                //             "X-Requested-With": "XMLHttpRequest"
+                //         },
+                //         body: JSON.stringify(data)
+                //     }).then(function(response){
+                //         console.log(response);
+                //     }).catch(function(error) {
 
-                        if(error.response.status == '422'){
+                //         if(error.response.status == '422'){
     
+                //             let errors = error.response.data.errors;      
+                //             let errorMessage = '';
+        
+                //             Object.keys(errors).forEach(function(key) {
+                //                 errorMessage += '<li>' + errors[key] + '</li>';
+                //             })
+            
+                //             console.log(errorMessage);
+                //         }
+    
+                //         if(error.response.status == '500'){
+                //             console.log(error);
+                //         }
+                //     });
+
+
+                    let response = await axios.post(url, data).then(response => {
+
+                        console.log(response);
+
+                    }).catch(error => {
+                        
+                        if(error.response.status == '422'){
+
                             let errors = error.response.data.errors;      
                             let errorMessage = '';
-        
+
                             Object.keys(errors).forEach(function(key) {
                                 errorMessage += '<li>' + errors[key] + '</li>';
                             })
-            
+
                             console.log(errorMessage);
                         }
-    
+
                         if(error.response.status == '500'){
                             console.log(error);
                         }
                     });
-
-
-                    // let response = await axios.post(url, data, {
-                    //     headers: {
-                    //         "Content-Type": "application/json",
-                    //         "Accept": "application/json, text-plain, */*",
-                    //         "X-Requested-With": "XMLHttpRequest",
-                    //     }
-                    // }).then(response => {
-
-                    //     console.log(response);
-
-                    // }).catch(error => {
-                        
-                    //     if(error.response.status == '422'){
-
-                    //         let errors = error.response.data.errors;      
-                    //         let errorMessage = '';
-
-                    //         Object.keys(errors).forEach(function(key) {
-                    //             errorMessage += '<li>' + errors[key] + '</li>';
-                    //         })
-
-                    //         console.log(errorMessage);
-                    //     }
-
-                    //     if(error.response.status == '500'){
-                    //         console.log(error);
-                    //     }
-                    // });
                 };
         
                 sendPostRequest();
