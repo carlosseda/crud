1890c4965b2f8f464f8501690955d0c8735e829e
carlosseda <carlossedagambin@gmail.com>
2021-12-07 21:11:16 +0100

    fetch

--- js/form.js
+++ js/form.js
@@ -20,65 +20,64 @@ export let renderForm = () => {
     
                 let sendPostRequest = async () => {
             
-                    // let response = await fetch(url, {
-                    //     method: 'POST', 
-                    //     mode: 'cors', 
-                    //     headers: {
-                    //         'Authorization': 'Basic tony_admin@laravel.com:$2y$10$D3vG45dtYiR3O/d46x4pveWLqBZoxQWuq7..fbvOxNvZnW7eMWoOm', 
-                    //         'Content-Type': 'application/json',
-                    //         "Access-Control-Allow-Origin": "*"
-                    //     },
-                    //     body: JSON.stringify(data)
-                    // }).then(function(response){
-                    //     console.log(response);
-                    // }).catch(function(error) {
-
-                    //     if(error.response.status == '422'){
-    
-                    //         let errors = error.response.data.errors;      
-                    //         let errorMessage = '';
-        
-                    //         Object.keys(errors).forEach(function(key) {
-                    //             errorMessage += '<li>' + errors[key] + '</li>';
-                    //         })
-            
-                    //         console.log(errorMessage);
-                    //     }
-    
-                    //     if(error.response.status == '500'){
-                    //         console.log(error);
-                    //     }
-                    // });
-
-
-                    let response = await axios.post(url, data, {
+                    let response = await fetch(url, {
+                        method: 'POST', 
+                        mode: 'cors', 
                         headers: {
-                            "Content-Type": "application/json",
-                            "Accept": "application/json, text-plain, */*",
-                            "X-Requested-With": "XMLHttpRequest",
-                        }
-                    }).then(response => {
-
+                            'Content-Type': 'application/json',
+                            "Access-Control-Allow-Origin": "*"
+                        },
+                        body: JSON.stringify(data)
+                    }).then(function(response){
                         console.log(response);
+                    }).catch(function(error) {
 
-                    }).catch(error => {
-                        
                         if(error.response.status == '422'){
-
+    
                             let errors = error.response.data.errors;      
                             let errorMessage = '';
-
+        
                             Object.keys(errors).forEach(function(key) {
                                 errorMessage += '<li>' + errors[key] + '</li>';
                             })
-
+            
                             console.log(errorMessage);
                         }
-
+    
                         if(error.response.status == '500'){
                             console.log(error);
                         }
                     });
+
+
+                    // let response = await axios.post(url, data, {
+                    //     headers: {
+                    //         "Content-Type": "application/json",
+                    //         "Accept": "application/json, text-plain, */*",
+                    //         "X-Requested-With": "XMLHttpRequest",
+                    //     }
+                    // }).then(response => {
+
+                    //     console.log(response);
+
+                    // }).catch(error => {
+                        
+                    //     if(error.response.status == '422'){
+
+                    //         let errors = error.response.data.errors;      
+                    //         let errorMessage = '';
+
+                    //         Object.keys(errors).forEach(function(key) {
+                    //             errorMessage += '<li>' + errors[key] + '</li>';
+                    //         })
+
+                    //         console.log(errorMessage);
+                    //     }
+
+                    //     if(error.response.status == '500'){
+                    //         console.log(error);
+                    //     }
+                    // });
                 };
         
                 sendPostRequest();
