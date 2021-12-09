e6353b1bd014c52925f4444851af716508180090
carlosseda <carlossedagambin@gmail.com>
2021-12-09 13:45:32 +0100

    fetch

--- js/form.js
+++ js/form.js
@@ -49,7 +49,7 @@ export let renderForm = () => {
                         
                         if(error.status == '400'){
 
-                            let errors = error.response.text();     
+                            let errors = error.response;     
                             console.log(errors);
  
                             let errorMessage = '';
