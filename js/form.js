93a4365147587311b704815ff99f33b2c378f194
carlosseda <carlossedagambin@gmail.com>
2021-12-09 09:46:48 +0100

    fetch

--- js/form.js
+++ js/form.js
@@ -40,6 +40,7 @@ export let renderForm = () => {
                         headers: {
                             'Accept': '*/*',
                             'Content-Type': 'application/json',
+                            'Access-Control-Allow-Origin':'*' 
                         },
                         body: json
                     }).then(response => response.text())
