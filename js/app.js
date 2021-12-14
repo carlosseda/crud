e3af1cb38117ac064fa626e2b39e75f549b198b1
carlosseda <carlossedagambin@gmail.com>
2021-12-14 21:26:22 +0100

    menu component

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 import {renderTabs} from './tabs.js';
 import {renderForm} from './form.js';
 import {renderCkeditor} from './ckeditor.js';
-import './table.js'
+import './table.js';
+import './menu.js'
 
 renderTabs();
 renderForm();
