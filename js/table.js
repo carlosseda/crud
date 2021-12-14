271ee92fdbb2409c3408addba586db1b6b4b162c
carlosseda <carlossedagambin@gmail.com>
2021-12-14 09:03:34 +0100

    table webcomponent

--- js/table.js
+++ js/table.js
@@ -1,75 +1,8 @@
-//Create a webcomponent to show a table, with a json from a fetch:
-// {
-//     "success": true,
-//     "data": [
-//         {
-//             "id": 18,
-//             "name": "Carlos",
-//             "email": "carlossedagambin@gmail.com",
-//             "email_verified_at": null,
-//             "created_at": "2021-12-11 12:31:57",
-//             "updated_at": "2021-12-11 12:31:57",
-//             "deleted_at": null,
-//             "roles": [
-//                 {
-//                     "id": 1,
-//                     "name": "Admin",
-//                     "guard_name": "api",
-//                     "created_at": "2021-12-09 10:37:24",
-//                     "updated_at": "2021-12-09 10:37:24",
-//                     "pivot": {
-//                         "model_id": 18,
-//                         "role_id": 1,
-//                         "model_type": "App\\User"
-//                     }
-//                 }
-//             ]
-//         },
-//         {
-//             "id": 19,
-//             "name": "luciano aras",
-//             "email": "latardito@gmail.com",
-//             "email_verified_at": null,
-//             "created_at": "2021-12-13 14:43:33",
-//             "updated_at": "2021-12-13 14:43:33",
-//             "deleted_at": null,
-//             "roles": [
-//                 {
-//                     "id": 1,
-//                     "name": "Admin",
-//                     "guard_name": "api",
-//                     "created_at": "2021-12-09 10:37:24",
-//                     "updated_at": "2021-12-09 10:37:24",
-//                     "pivot": {
-//                         "model_id": 19,
-//                         "role_id": 1,
-//                         "model_type": "App\\User"
-//                     }
-//                 }
-//             ]
-//         }
-//     ],
-//     "message": "Users retrieved successfully."
-// }
-
 class Table extends HTMLElement {
 
     constructor() {
         super();
         this.shadow = this.attachShadow({ mode: 'open' });
-        this.shadow.innerHTML = `
-        <style>
-            table {
-                border-collapse: collapse;
-                width: 100%;
-            }
-            td, th {
-                border: 1px solid hsl(0, 0%, 87%);
-                color: hsl(0, 0%, 100%);
-                padding: 8px;
-                text-align: left;
-            }
-        </style>`;
     }
 
     connectedCallback() {
@@ -94,7 +27,20 @@ class Table extends HTMLElement {
 
     render() {
 
-        this.shadow.innerHTML = `
+        this.shadow.innerHTML = 
+        `
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
+            td, th {
+                border: 1px solid hsl(0, 0%, 87%);
+                color: hsl(0, 0%, 100%);
+                padding: 8px;
+                text-align: left;
+            }
+        </style>
         <table>
             <thead>
                 ${this.getTableHeader()}
